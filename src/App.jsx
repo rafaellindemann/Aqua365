82f0cb46a3f60851268973968e6ea5809975201b
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-06-28 16:56:35 -0300

    feat: login/logoff concluídos

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,3 @@ function App() {
 }
 
 export default App
-
-
-
-
-// import { Outlet } from 'react-router-dom'
-// import './App.css'
-// import Navbar from './components/Navbar'
-// import { GlobalContextProvider } from './context/GlobalContext'
-
-// function App() {
-
-//   return (
-//     <GlobalContextProvider>
-//       <Navbar />
-//       <Outlet />
-//     </GlobalContextProvider>
-//   )
-// }
-
-// export default App
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,14 +3,12 @@ import { Navigate } from 'react-router-dom';
 import { GlobalContext } from '../context/GlobalContext';
 
 const ProtectedRoute = ({ children }) => {
-  const { isLoggedIn } = useContext(GlobalContext); // Supondo que você tenha um estado que indique se o usuário está logado
+  const { isLoggedIn } = useContext(GlobalContext);
 
   if (!isLoggedIn) {
-    // Se o usuário não está logado, redirecione para a página de login
     return <Navigate to="/login" />;
   }
 
-  // Se o usuário está logado, renderize o componente filho
   return children;
 }
 
